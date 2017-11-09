fc30cb03eb5109c4fffa1a66db0617cea6738f52
leo93921 <leonardo.capoccia@hotmail.it>
2017-11-09 12:55:15 +0100

    Add constants for offer states

--- src/main/java/it/fyb/Utlis/FYBConstants.java
+++ src/main/java/it/fyb/Utlis/FYBConstants.java
@@ -13,9 +13,16 @@ public class FYBConstants {
     public static String USER_TOKEN = "psc";
 
     // If there are less than MAX_DAYS_FOR_REFUND, then the refund is
-    // only the REFUND_PERCENTAGE_AFTER_MAX_DAYS of the total sale
     public static final Integer MAX_DAYS_FOR_REFUND = 7;
+    // only the REFUND_PERCENTAGE_AFTER_MAX_DAYS of the total sale
     public static final double REFUND_PERCENTAGE_AFTER_MAX_DAYS = 0.5;
 
     public static final Integer MILLISECONDS_IN_A_DAY = 86400000;
+
+    public static final String OFFER_ACCEPTED = "ACCEPTED";
+    public static final String OFFER_NOT_ACCEPTED = "NOT_ACCEPTED";
+    public static final String OFFER_PAID = "PAID";
+    public static final String OFFER_NOT_PAID = "NOT_PAID";
+    public static final String OFFER_REFUNDED = "REFUNDED";
+
 }
